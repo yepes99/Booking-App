997fb62c59f1b2eaa5fa325b1e9e8d648bc8a0e4
yepes99 <kodasouttakes@gmail.com>
2024-12-03 11:43:23 +0100

    navbar js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
    
-     @vite(['resources/css/app.css', 'resources/js/app.js'])
+     @vite(['resources/css/app.css', 'resources/js/app.js','resources/js/navbar.js'])
 </head>
 <body>
     <header>
